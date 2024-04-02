37e89e182ce2de0dc532efc4bb7b96b1c21a75fc
adoronin158@gmail.com <adoronin158@gmail.com>
2024-04-02 18:53:21 +0300

    Revert "finished"
    This reverts commit d46b8df2a9f0b6424386288a8ab84b06771ba4aa.

--- FavouriteDishBot/Util/String/SystemStringsStorage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FavouriteDishBot.Util.String;
-
-public class SystemStringsStorage
-{
-    public const string CommandStart = "/start";
-    public const string CommandReset = "/reset";
-    public const string TelegramToken = "7110426275:AAH7EEbnhcafTEyqkq18lb3ROQxz03KUY9E";
-}
